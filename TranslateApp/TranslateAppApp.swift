adfdc1f2fc6b103146c82a89d424d49afcaaaff9
akotetsu <yuanliqu@gmail.com>
2025-07-01 19:59:30 +0900

    Initial Commit

---
+++ TranslateApp/TranslateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateAppApp.swift
+//  TranslateApp
+//
+//  Created by 原里駆 on 2025/07/01.
+//
+
+import SwiftUI
+
+@main
+struct TranslateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
