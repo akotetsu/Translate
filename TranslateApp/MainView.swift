0fbe69257d009e3b618c2760b97c9fde1ff6c81d
akotetsu <yuanliqu@gmail.com>
2025-07-02 16:06:03 +0900

    メイン画面の大まかなレイアウト

--- TranslateApp/MainView.swift
+++ TranslateApp/MainView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var promptText = ""
+    
     var body: some View {
-        Text("ここは入力エリアを想定したメイン画面です。")
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+            VStack {
+                Text("Original Text")
+                    .font(.title)
+                
+                
+                TextEditor(text: $promptText)
+                    .padding()
+                    .frame(width: .infinity, height: 400)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                //.padding(.horizontal)
+                
+                Button {
+                    print("翻訳する")
+                } label: {
+                    Text("Translate")
+                        .font(.headline)
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 32)
+                        .background(Color(.systemMint))
+                        .foregroundStyle(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
+            .padding()
+        }
     }
 }
 
